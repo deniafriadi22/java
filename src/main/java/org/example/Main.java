5be389152a182fcab4b3d7a6e215c733482d71c7
deni <deniafriadi22@gmail.com>
2024-03-06 10:29:10 +0700

    Latihan java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import javax.xml.transform.Result;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -13,14 +15,59 @@ public class Main {
         //variable
         boolean flag;
         float run;
-        for (int i = 1; i <= 5; i++){
-            System.out.println("i = " +1);
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + 1);
+        }
+        int a = 10;
+        int b = 2;
+        float c = 3;
+        String nama = "Deni";
+        char q = 'y';
+        boolean flag2 = false;
+
+        float result;
+        //int result; //sebelumnya int diubah jadi float
+        //result = (a/c);
+        result = (int) (a/b);
+        System.out.println(result);
+
+        int timeStart = 8;
+        int timeFinish = 17;
+        int waktuSaya = 7;
+        int waktuPulang =12;
+        //karyawan tepat waktu datang dan pulang dapat selamat
+        if (waktuSaya <= timeStart && waktuPulang >= timeFinish) {
+            System.out.println("selamat kamu tepat waktu");
+            System.out.println("Anda datang "+(timeStart - waktuSaya) + " lebih cepat");
+            System.out.println("Anda pulang"+(waktuPulang-timeFinish)+" jam lebih lama");
+            //karyawan datang duluan, pulang cepat di cengin
+        } else if (waktuSaya <= timeStart && waktuPulang <= timeFinish) {
+            System.out.println("datang awal pulang cepat nih ye");
+            //karyawan telat, pulang lama, dimaafkan
+        } else if (waktuSaya >= timeStart && waktuPulang >= timeFinish) {
+            System.out.println("dimaafkan datang telatnya");
+            //karyawan telat , pulang di soraki
+        } else {
+            System.out.println("Kamu Telat");
+            System.out.println("Kamu telat" + (waktuSaya - timeStart) + " jam");
+            System.out.println("anda pulang duluan" + (timeFinish - waktuPulang) + " jam lebih cepat");
         }
 
-//        for (int i = 1; i <= 5; i++) {
-//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-//            System.out.println("i = " + i);
-//        }
+        int day=3;
+        switch (day){
+            case 1:
+                System.out.println("saya wfo");
+                break;
+            case 2 :
+                System.out.println("saya libur/cuti");
+                break;
+            case 3:
+                System.out.println("saya wfh");
+                break;
+            default:
+                System.out.println("liburan alias weekend");
+                break;
+        }
+        System.out.println("hehehe");
     }
 }
--- src/main/java/org/example/ShapeDrawerPolyMorph.java
+++ src/main/java/org/example/ShapeDrawerPolyMorph.java
@@ -1,19 +1,51 @@
 package org.example;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
-public class ShapeDrawer {
+
+interface Shape{// one of imple of polymorh
+    void draw(int height, int side);
+}
+
+class drawTriangleSS implements Shape {
+
+    public void draw(int height, int side) {
+        //overriding (mengganti secara paksa content dari function dalam plymorp)
+        //nested loop
+        //outer loop (take care the rows)
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            //inner loop
+            System.out.println();
+
+        }
+    }
+}
+
+public class ShapeDrawerPolyMorph {
     Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu(){
+        System.out.println("selamat datang di ASCII gambar!");
+        System.out.println("pilih gambar");
+        System.out.println("1. Segitiga siku - siku");
+        System.out.println("2. Segitiga sama kaki");
+        System.out.println("3. Exit");
+    }
     public void drawShape(int choice){
         //conditional menu
         switch (choice){
             case 1:
                 System.out.println("masukan berapa tinggi segitiga");
-                int tinggiSegitiga = scanner.nextInt();
-                drawTriangleSS(tinggiSegitiga);
+                int height = scanner.nextInt();
+                new drawTriangleSS().draw(height, 1);
                 break;
             case 2:
                 System.out.println("masukan berapa tinggi segitiga");
-                int tinggiSegitiga2 = scanner.nextInt();
-                drawTriangleSK(tinggiSegitiga2);
+                int height2 = scanner.nextInt();
+                new drawTriangleSK().draw(height2, 1);
                 break;
             case 3:
                 break;
@@ -22,12 +54,12 @@ public class ShapeDrawer {
         }
 
     }
-
-    public void drawTriangleSK(int tinggiSegitiga2) {
+    class drawTriangleSK implements Shape{
+        public void draw(int height, int side) {
         //nested loop
         //outer loop (take care the rows)
-        for(int i = 1; i <= tinggiSegitiga2; i++){
-            for (int j = 1; j <= tinggiSegitiga2 - i; j++){
+        for(int i = 1; i <= height; i++){
+            for (int j = 1; j <= height - i; j++){
                 System.out.print(" ");
             }
             for (int k = 1; k <= 2 * i - 1; k++){
@@ -38,43 +70,28 @@ public class ShapeDrawer {
 
         }
     }
-
-    public void drawTriangleSS(int tinggiSegitiga) {
-        //nested loop
-        //outer loop (take care the rows)
-        for(int i = 1; i <= tinggiSegitiga;i++){
-            for (int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-            //inner loop
-            System.out.println();
-
-        }
     }
 
-
-    public void displayMenu(){
-        System.out.println("selamat datang di ASCII gambar!");
-        System.out.println("pilih gambar");
-        System.out.println("1. Segitiga siku - siku");
-        System.out.println("2. Segitiga sama kaki");
-        System.out.println("3. Exit");
-    }
 
     public void run (){
         //loop while untuk aplikasi terus running sampai ada exit
         while (true){
             displayMenu();
             System.out.println("Masukan pilihan anda");
-            int choice = scanner.nextInt();
-            drawShape(choice); //dimana ada kondisional (if else atau switch case)
-            System.out.println();
+            try {
+                int choice = scanner.nextInt();
+                drawShape(choice); //dimana ada kondisional (if else atau switch case)
+                System.out.println();
+            } catch (InputMismatchException ime) {
+                System.out.println("Please enter a valid integer");
+                scanner.next();
+            }
         }
     }
     public static void main (String[] args){
         //Inherintance (oop)
         //untuk running
-        ShapeDrawer drawer = new ShapeDrawer();
+        ShapeDrawerPolyMorph drawer = new ShapeDrawerPolyMorph();
         drawer.run();
     }
 }
